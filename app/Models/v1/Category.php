df091a6eb36fe563f0b692d16249bc23d214f1f3
Mian Ahissan <mianahissan@gmail.com>
2022-05-12 20:13:53 +0000

    master

--- app/Models/v1/Category.php
+++ app/Models/v1/Category.php
@@ -9,12 +9,11 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
 use JetBrains\PhpStorm\ArrayShape;
-use phpDocumentor\Reflection\Types\Boolean;
-use stdClass;
 
 /**
  * @property mixed $label
  * @property bool|mixed $description
+ * @method static find()
  */
 class Category extends Model
 {
@@ -29,7 +28,7 @@ class Category extends Model
         ];
     }
 
-    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function create(Request $request): array
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function createCategory(Request $request): array
     {
         try {
             if (Validator::make($request->all(), [
@@ -47,6 +46,53 @@ class Category extends Model
                 }
             }
             return $this->responseModel(false, [], "label is required");
+        }catch (Exception $e){
+            return $this->responseModel(false, [], $e);
+        }
+    }
+
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function updateCategory(Request $request): array
+    {
+        try {
+            if (Validator::make($request->all(), [
+                'label' => 'required',
+            ])){
+                $category = Category::find($request->input('id'));
+                if (!$category) return $this->responseModel(false, [], `category does not exist`); else{
+                    $category->label = $request->input();
+                    $category->description = $request->input('description') && $request->input('description');
+                    $category->save();
+                    return $this->responseModel(true, $category);
+                }
+            }
+            return $this->responseModel(false, [], "label is required");
+        }catch (Exception $e){
+            return $this->responseModel(false, [], $e);
+        }
+    }
+
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function getAllCategory(): array
+    {
+        try {
+            return $this->responseModel(true, Category::all());
+        }catch (Exception $e){
+            return $this->responseModel(false, [], $e);
+        }
+    }
+
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function getCategoryById($id): array
+    {
+        try {
+            return $this->responseModel(true, Category::find($id));
+        }catch (Exception $e){
+            return $this->responseModel(false, [], $e);
+        }
+    }
+
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function deleteCategoryById($id): array
+    {
+        try {
+            return $this->responseModel(true, Category::find($id)->deleted());
         }catch (Exception $e){
             return $this->responseModel(false, [], $e);
         }
--- app/Models/v1/Faq.php
+++ app/Models/v1/Faq.php
@@ -2,10 +2,100 @@
 
 namespace App\Models\v1;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use JetBrains\PhpStorm\ArrayShape;
 
+/**
+ * @method static find($id)
+ * @property mixed $question
+ * @property bool|mixed $answer
+ */
 class Faq extends Model
 {
     use HasFactory;
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] private function responseModel($status = false, $object = [], $error = null): array
+    {
+        return [
+            'status' => $status,
+            'object' => $object,
+            'error' => $error
+        ];
+    }
+
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function createFaq(Request $request): array
+    {
+        try {
+            if (Validator::make($request->all(), [
+                'question' => 'required',
+                'answer'=> 'required'
+            ])){
+                $question = DB::table('categories')
+                    ->where('question', $request->input('question'))
+                    ->first();
+                if ($question) return $this->responseModel(false, [], `Question $question already exist`); else{
+                    $faq = new Faq();
+                    $faq->question = $request->input();
+                    $faq->answer = $request->input('answer') && $request->input('answer');
+                    $faq->save();
+                    return $this->responseModel(true, $faq);
+                }
+            }
+            return $this->responseModel(false, [], "Question & answer is required");
+        }catch (Exception $e){
+            return $this->responseModel(false, [], $e);
+        }
+    }
+
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function updateFaq(Request $request): array
+    {
+        try {
+            if (Validator::make($request->all(), [
+                'question' => 'required',
+                'answer'=> 'required'
+            ])){
+                $faq = Faq::find($request->input('id'));
+                if (!$faq) return $this->responseModel(false, [], `Question does not exist`); else{
+                    $faq->question = $request->input();
+                    $faq->answer = $request->input('answer') && $request->input('answer');
+                    $faq->save();
+                    return $this->responseModel(true, $faq);
+                }
+            }
+            return $this->responseModel(false, [], "Question & answer is required");
+        }catch (Exception $e){
+            return $this->responseModel(false, [], $e);
+        }
+    }
+
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function getAllFaq(): array
+    {
+        try {
+            return $this->responseModel(true, Faq::all());
+        }catch (Exception $e){
+            return $this->responseModel(false, [], $e);
+        }
+    }
+
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function getFaqById($id): array
+    {
+        try {
+            return $this->responseModel(true, Faq::find($id));
+        }catch (Exception $e){
+            return $this->responseModel(false, [], $e);
+        }
+    }
+
+    #[ArrayShape(['status' => "string", 'object' => "null", 'error' => "null"])] public function deleteFaqById($id): array
+    {
+        try {
+            return $this->responseModel(true, Faq::find($id)->deleted());
+        }catch (Exception $e){
+            return $this->responseModel(false, [], $e);
+        }
+    }
 }
