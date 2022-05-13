b65092b69978af725c62c6ce26b8dd441f57180c
Mian Ahissan <mianahissan@gmail.com>
2022-05-13 00:58:18 +0000

    devgu

---
+++ database/factories/v1/FaqFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories\v1;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\v1\Faq>
+ */
+class FaqFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'question' => $this->faker->unique()->text(),
+            'answer' => $this->faker->text,
+            'order' => $this->faker->unique()->randomNumber()
+        ];
+    }
+}
