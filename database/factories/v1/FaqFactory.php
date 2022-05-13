ecda34e183e6350ad71605d61d3263fe737f324f
Mian Ahissan <mianahissan@gmail.com>
2022-05-13 11:56:45 +0000

    devgu

--- database/factories/v1/FaqFactory.php
+++ database/factories/v1/FaqFactory.php
@@ -5,7 +5,7 @@ namespace Database\Factories\v1;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\v1\Faq>
+ * @extends Factory
  */
 class FaqFactory extends Factory
 {
