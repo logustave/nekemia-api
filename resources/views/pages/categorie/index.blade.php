9353a01b3140aa9ae58a3a5174d3158654200a59
noewilly <w.djedje@panelyscash.com>
2022-05-14 01:49:09 +0000

    for devgu

--- resources/views/pages/categorie/index.blade.php
+++ resources/views/pages/categorie/index.blade.php
@@ -15,9 +15,8 @@
                     <ul class="list-group listElement">
                         <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg element">
                             <div class="d-flex flex-column">
-                                <h6 class="mb-1 text-dark font-weight-bold text-sm">le label</h6>
-                                <span class="text-xs">description</span>
-                            </div>
+                                <h6 class="mb-1 text-dark font-weight-bold">le label</h6>
+                                <span class="text-xs">description mettre un sub str apres 200 carracteres </span>                            </div>
                             <div class="d-flex align-items-center text-sm ">
                                 <small class="mx-1">01/01/2022</small>
                                 <a class="px-3 mb-0 btn btn-outline-primary" href="categorie/information/1"
--- resources/views/pages/faq/index.blade.php
+++ resources/views/pages/faq/index.blade.php
@@ -15,12 +15,12 @@
                     <ul class="list-group listElement">
                         <li class="list-group-item border-0 d-flex justify-content-between ps-0 mb-2 border-radius-lg element">
                             <div class="d-flex flex-column">
-                                <h6 class="mb-1 text-dark font-weight-bold text-sm">le label</h6>
-                                <span class="text-xs">description</span>
+                                <h6 class="mb-1 text-dark font-weight-bold ">Question ?</h6>
+                                <span class="text-xs">reponse mettre un sub str apres 200 carracteres </span>
                             </div>
                             <div class="d-flex align-items-center text-sm ">
                                 <small class="mx-1">01/01/2022</small>
-                                <a class="px-3 mb-0 btn btn-outline-primary" href="categorie/information/1"
+                                <a class="px-3 mb-0 btn btn-outline-primary" href="faq/information/1"
                                    type="button">Voir</a>
                             </div>
                         </li>
@@ -33,17 +33,17 @@
         <div class="col-xl-4 col-lg-4 col-md-6 mx-auto formSave">
             <div class="card z-index-0">
                 <div class="card-header text-center pt-4">
-                    <h5>Enregistrer une categorie</h5>
+                    <h5>Enregistrer une Question</h5>
                 </div>
 
                 <div class="card-body">
                     <form role="form text-left">
                         <div class="mb-3">
-                            <input type="text" name="label" class="form-control" placeholder="label" aria-label="Name"
-                                   aria-describedby="email-addon">
+                            <input type="text" name="question" class="form-control" placeholder="question ?" aria-label="Name"
+                                   >
                         </div>
                         <div class="mb-3">
-                            <textarea name="description" class="form-control" placeholder="Description"
+                            <textarea name="answer" class="form-control" placeholder="Reponse"
                                       style="resize: none" rows="5"></textarea>
                         </div>
 
