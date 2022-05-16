942924aaf73b0e807eff4a24cabb85860f0c9f00
Mian Ahissan <mianahissan@gmail.com>
2022-05-16 01:34:43 +0000

    devgu

--- app/Http/Controllers/v1/FaqController.php
+++ app/Http/Controllers/v1/FaqController.php
@@ -161,69 +161,69 @@ class FaqController extends Controller
     }
 
 
-    /**
-     * @OA\Get(
-     *      path="/v1/faq/{id}",
-     *      operationId="getFaqByIdAPI",
-     *      tags={"FAQ"},
-     *      summary="GET FAQ",
-     *      description="GET FAQ",
-     *      security={{"bearerAuth":{}}},
-     *  @OA\Parameter(
-     *      name="id",
-     *      in="path",
-     *      required=true,
-     *      @OA\Schema(
-     *           type="integer"
-     *      )
-     *   ),
-     *     @OA\Response(
-     *          response=200,
-     *          description="Opération éffectuée",
-     *          @OA\JsonContent(
-     *              @OA\Property(property="status", type="boolean"),
-     *              @OA\Property(
-     *                  property="object",
-     *                  type="array",
-     *                  @OA\Items(
-     *                      @OA\Property(property="id", type="integer", format="number", example="0"),
-     *                      @OA\Property(property="question", type="string"),
-     *                      @OA\Property(property="answer", type="string"),
-     *                      @OA\Property(property="order", type="integer"),
-     *                      @OA\Property(property="created_at", type="string"),
-     *                      @OA\Property(property="update_at", type="string"),
-     *                      @OA\Property(property="delete_at", type="string"),
-     *                  ),
-     *              ),
-     *          @OA\Property(property="error", type="string"),
-     *          ),
-     *      ),
-     *      @OA\Response(
-     *          response=401,
-     *          description="Non authentifié",
-     *      ),
-     *      @OA\Response(
-     *          response=403,
-     *          description="Interdit"
-     *      ),
-     * @OA\Response(
-     *      response=400,
-     *      description="Mauvaise demande"
-     *   ),
-     * @OA\Response(
-     *      response=404,
-     *      description="pas trouvé"
-     *   ),
-     *  )
-     */
-    public function getFaqByIdAPI($id): JsonResponse
-    {
-        $faq = (new Faq)->getFaqById($id);
-        return response()->json(
-            $faq,
-            200,
-            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
-            $this->format
-        );
-    }
+//    /**
+//     * @OA\Get(
+//     *      path="/v1/faq/{id}",
+//     *      operationId="getFaqByIdAPI",
+//     *      tags={"FAQ"},
+//     *      summary="GET FAQ",
+//     *      description="GET FAQ",
+//     *      security={{"bearerAuth":{}}},
+//     *  @OA\Parameter(
+//     *      name="id",
+//     *      in="path",
+//     *      required=true,
+//     *      @OA\Schema(
+//     *           type="integer"
+//     *      )
+//     *   ),
+//     *     @OA\Response(
+//     *          response=200,
+//     *          description="Opération éffectuée",
+//     *          @OA\JsonContent(
+//     *              @OA\Property(property="status", type="boolean"),
+//     *              @OA\Property(
+//     *                  property="object",
+//     *                  type="array",
+//     *                  @OA\Items(
+//     *                      @OA\Property(property="id", type="integer", format="number", example="0"),
+//     *                      @OA\Property(property="question", type="string"),
+//     *                      @OA\Property(property="answer", type="string"),
+//     *                      @OA\Property(property="order", type="integer"),
+//     *                      @OA\Property(property="created_at", type="string"),
+//     *                      @OA\Property(property="update_at", type="string"),
+//     *                      @OA\Property(property="delete_at", type="string"),
+//     *                  ),
+//     *              ),
+//     *          @OA\Property(property="error", type="string"),
+//     *          ),
+//     *      ),
+//     *      @OA\Response(
+//     *          response=401,
+//     *          description="Non authentifié",
+//     *      ),
+//     *      @OA\Response(
+//     *          response=403,
+//     *          description="Interdit"
+//     *      ),
+//     * @OA\Response(
+//     *      response=400,
+//     *      description="Mauvaise demande"
+//     *   ),
+//     * @OA\Response(
+//     *      response=404,
+//     *      description="pas trouvé"
+//     *   ),
+//     *  )
+//     */
+//    public function getFaqByIdAPI($id): JsonResponse
+//    {
+//        $faq = (new Faq)->getFaqById($id);
+//        return response()->json(
+//            $faq,
+//            200,
+//            ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+//            $this->format
+//        );
+//    }
 }
