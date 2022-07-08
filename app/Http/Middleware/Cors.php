e6b1ad124543655cfeeefdf29080093c6f98d4e2
Mian Ahissan <g.mian@panelyscash.com>
2022-07-08 20:13:18 +0000

    Resolve COrs Origin

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -21,6 +21,6 @@ class Cors
         return $next($request)
             ->header('Access-Control-Allow-Origin', "*")
             ->header('Access-Control-Allow-Methods', "GET, POST, PUT, DELETE, PATCH, OPTIONS")
-            ->header('Access-Control-Allow-Headers', "Accept, Origin, Access-Control-Allow-Credentials, X-Requested-With, Content-Type, Authorization");
+            ->header('Access-Control-Allow-Credentials', "true");
     }
 }
