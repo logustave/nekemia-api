bb972963cf4a2fe3aa07f92ab5e699a48976e03e
Mian Ahissan <g.mian@panelyscash.com>
2022-07-08 18:33:45 +0000

    master

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,9 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'localhost',
+        'localhost:3000',
+        '*.nekemia.com',
+        '*.nekemia-api.test'
     ];
 }
--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['localhost', 'localhost:3000', '*.nekemia.com', '*.nekemia-api.test'],
 
     'allowed_origins_patterns' => [],
 
