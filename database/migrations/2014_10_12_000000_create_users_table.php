96f74d870f0d3068d6e3457002a2e9d4b96c31e8
logustave <mianahissan@proton.me>
2023-03-24 18:27:46 +0000

    change database long

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name', 191);
-            $table->string('email', 255)->unique();
+            $table->string('email', 191)->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password', 191);
             $table->rememberToken();
